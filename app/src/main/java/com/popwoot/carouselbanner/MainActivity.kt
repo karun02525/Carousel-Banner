c35673623fd87c8cee9af0d14c2227cf8c986b93
kaju02525 <karunkumar02525@gmail.com>
2020-05-09 23:14:20 +0530

    fixed bugs

--- app/src/main/java/com/popwoot/carouselbanner/MainActivity.kt
+++ app/src/main/java/com/popwoot/carouselbanner/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         list.add("https://akm-img-a-in.tosshub.com/indiatoday/images/story/201905/AKSHAY1.jpeg?HAKoXX_ba8nMjk4Vo.AOXOxLFBo7E6oY")
         list.add("https://en.janbharattimes.com/wp-content/uploads/2019/01/salman-khan-movies.jpg")
         list.add("https://static.asianetnews.com/images/01dax9bpf2hyymhqtbbst7p7tm/--15--1967------------_710x400xt.jpg")
-        list.add("https://www.itl.cat/pngfile/big/86-867177_south-indian-heroine-wallpaper-sabse-badhkar-hum-2.jpg")
+        list.add("https://resize.indiatvnews.com/en/resize/newbucket/715_-/2020/01/varun-shraddha-1579784741.jpg")
         list.add("https://qph.fs.quoracdn.net/main-qimg-a92551a9b90b6a9df5150157f4ef9ac5.webp")
 
         banner.setOnCarouselItemChangeListener {
